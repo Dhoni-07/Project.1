ef1967530a611303fdd6320136130c8759b2ea3a
s salam <salamrpsssd@gmail.com>
2025-02-06 13:03:33 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white mt-20 py-8">
+      <div className="container mx-auto px-4 text-center">
+        <p>© 2024 TechSolutions. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
